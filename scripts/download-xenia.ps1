814139ce3a7ba925a2ed7548e40fb5de2d8f71ce
Clément Dreptin <cdreptin@gmail.com>
2024-02-14 03:48:25 +0100

    refactor(scripts): changed download-xenia script to stop taking a parameter and requiring an unrestricted execution policy

--- scripts/download-xenia.ps1
+++ scripts/download-xenia.ps1
@@ -1,13 +1,7 @@
-param([string]$XeniaDir)
-
-$XeniaExePath = "$($XeniaDir)\xenia.exe"
-$XeniaZipPath = "$($XeniaDir)\xenia.zip"
+$XeniaDir = "$PSScriptRoot\..\tools\xenia"
+$XeniaExePath = "$XeniaDir\xenia.exe"
+$XeniaZipPath = "$XeniaDir\xenia.zip"
 $XeniaDownloadUri = "https://github.com/xenia-project/release-builds-windows/releases/latest/download/xenia_master.zip"
-
-if (!$XeniaDir) {
-    echo "You need to provide a XeniaDir argument!"
-    Exit 1
-}
 
 # Exit early if the file already exists
 if ([System.IO.File]::Exists($XeniaExePath)) {
@@ -16,29 +10,29 @@ if ([System.IO.File]::Exists($XeniaExePath)) {
 
 # Download the latest Xenia binary
 try {
-    $null = mkdir -Force $XeniaDir
+    $Null = mkdir -Force $XeniaDir
     $WebClient = New-Object System.Net.WebClient
     $WebClient.DownloadFile($XeniaDownloadUri, $XeniaZipPath)
-} catch {
-    echo "Could not download the Xenia binary!"
-    echo $_.Exception
+}
+catch {
+    Write-Host -ForegroundColor Red "Could not download the Xenia binary!"
     Exit 1
 }
 
 # Unzip
 try {
     Expand-Archive $XeniaZipPath -Destination $XeniaDir
-} catch {
-    echo "Could not unzip $($XeniaZipPath)"
-    echo $_.Exception
+}
+catch {
+    Write-Host -ForegroundColor Red "Could not unzip $XeniaZipPath"
     Exit 1
 }
 
 # Delete the zip file
 try {
     Remove-Item $XeniaZipPath
-} catch {
-    echo "Could not delete $($XeniaZipPath)"
-    echo $_.Exception
+}
+catch {
+    Write-Host -ForegroundColor Red "Could not delete $XeniaZipPath"
     Exit 1
 }
